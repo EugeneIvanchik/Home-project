343ab347a4432f5d31df06cf21d8bcbe38f2ffb0
EugeneIvanchik <EugeneIvanchik@gmail.com>
2021-04-18 13:12:02 +0300

    Create Registration.cs

---
+++ Registration.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace Registration
+{
+    [TestFixture]
+    public class Registration
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private IWebElement dropdownElement;
+        private SelectElement dropdown;
+        private string email = "E91dot@gmail.com";
+        private string password = "123qweQWE!@#";
+        private void byNameSendKeys(string locatorName, string keyValue)
+        {
+            driver.FindElement(By.Name(locatorName)).SendKeys(keyValue);
+        }
+
+        private void chooseFromDropdown(string textValue)
+        {
+            dropdown = new SelectElement(dropdownElement);
+            dropdown.SelectByText(textValue);
+        }
+
+        [SetUp]
+
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        }
+
+        [Test]
+        public void RegistrationTest()
+        {
+            driver.Url = "http://localhost/litecart";
+            wait.Until(ExpectedConditions.TitleIs("Online Store | My Store"));
+            IWebElement registrationLink = driver.FindElement(By.XPath("//*[contains(text(), 'New customers click here')]"));
+            Thread.Sleep(500);
+            registrationLink.Click();
+            wait.Until(ExpectedConditions.TitleIs("Create Account | My Store"));
+            Thread.Sleep(500);
+            byNameSendKeys("firstname", "Eugene");
+            byNameSendKeys("lastname", "92dot");
+            byNameSendKeys("address1", "Pushkina str 10");
+            byNameSendKeys("postcode", "10000");
+            byNameSendKeys("city", "Minsk");
+
+            dropdownElement = driver.FindElement(By.CssSelector("select[name='country_code']"));
+            chooseFromDropdown("United States"); //правильный ли это подход: переменной dropdownElement
+                                                 //присваивать каждый раз новый dropdown на странице перед тем
+                                                 //как использовать метод по выбору значения из dropdown?
+
+            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("select[name='zone_code']")));
+
+            dropdownElement = driver.FindElement(By.CssSelector("select[name='zone_code']"));
+            chooseFromDropdown("California");
+
+            byNameSendKeys("email", email);
+            byNameSendKeys("phone", "+1200200000");
+            byNameSendKeys("password", password);
+            byNameSendKeys("confirmed_password", password);
+            driver.FindElement(By.Name("newsletter")).Click();
+            driver.FindElement(By.Name("create_account")).Click();
+
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='notices']/div[contains(text(), ' Your customer account has been created.')]")));
+
+            IWebElement logoutLink = driver.FindElement(By.XPath("//*[contains(text(), 'Logout')]"));
+            logoutLink.Click();
+
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='notices']/div[contains(text(), 'You are now logged out.')]")));
+
+            byNameSendKeys("email", email);
+            byNameSendKeys("password", password);
+            driver.FindElement(By.XPath("//*[@name='login']")).Click();
+
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='notices']/div[contains(text(), 'You are now logged in as')]")));
+
+            logoutLink = driver.FindElement(By.XPath("//*[contains(text(), 'Logout')]"));
+            logoutLink.Click();
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
