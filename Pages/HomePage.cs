c584334b03e7dd8236d98fc7f072d935e8901713
EugeneIvanchik <EugeneIvanchik@gmail.com>
2021-05-14 11:39:27 +0300

    Create HomePage.cs

---
+++ Pages/HomePage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace home_project
+{
+    public class HomePage : Page
+    {
+        internal HomePage(IWebDriver driver) : base(driver) { }
+
+        internal void Open()
+        {
+            driver.Url = "http://localhost/litecart";
+        }
+        internal void OpenFirstItem()
+        {
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='box-most-popular']//li[1]/a"))).Click();
+        }
+        internal void checkout()
+        {
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[contains(text(), 'Checkout')]"))).Click();
+        }
+
+    }
+}
