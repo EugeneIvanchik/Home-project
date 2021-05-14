33dd16e1652d892317926e838a7c51beb1a13f5c
EugeneIvanchik <EugeneIvanchik@gmail.com>
2021-05-14 11:39:35 +0300

    Create Page.cs

---
+++ Pages/Page.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace home_project
+{
+    public class Page
+    {
+        internal IWebDriver driver;
+        internal WebDriverWait wait;
+
+        public Page(IWebDriver driver)
+        {
+            this.driver = driver;
+            this.wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        }
+    }
+}
